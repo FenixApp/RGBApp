b41198f3fa96923487ee12a0b458d120bc6e1802
Dmitriy Starozhilov <dimstar-90@mail.ru>
2022-03-08 14:19:53 +0500

    Finish working project

--- RGBApp/ViewController.swift
+++ RGBApp/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var areaColors: UIView!
+    
+    @IBOutlet var valueRedSlider: UILabel!
+    @IBOutlet var valueGreenSlider: UILabel!
+    @IBOutlet var valueBlueSlider: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        areaColors.layer.cornerRadius = 15
+        
+        areaColors.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+        
+        valueRedSlider.text = String(format: "%.2f", redSlider.value)
+        valueGreenSlider.text = String(format: "%.2f", greenSlider.value)
+        valueBlueSlider.text = String(format: "%.2f", blueSlider.value)
+        
     }
-
-
+    
+    @IBAction func actionSliderRGB() {
+        areaColors.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+        
+        valueRedSlider.text = String(format: "%.2f", redSlider.value)
+        valueGreenSlider.text = String(format: "%.2f", greenSlider.value)
+        valueBlueSlider.text = String(format: "%.2f", blueSlider.value)
+    }
 }
-
